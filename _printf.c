a0e3d8653cdf10e62487ab311b3d9400036fa6f1
Derrick Ampire <ampire90@gmail.com>
2021-07-27 12:42:34 -0700

    PrintF Function

--- _printf.c
+++ _printf.c
@@ -2,6 +2,33 @@
 #include "holberton.h"
 
 /**
+ * check_for_specs - checks for valid format specifiers
+ * @format: format of specifiers
+ *
+ * Return: pointer to function or NULL
+ */
+static int (*check_for_specs(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
  * print_f - prints everything
  * @format: list of arguments
  *
