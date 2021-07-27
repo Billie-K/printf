073bf7a8f3ed0c6563d7e7359f92aa764767947e
Billie-K <billiemush@gmail.com>
2021-07-27 12:12:13 -0700

    bettyy docs

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include "holberton.h"
+
+/**
+* _printf - prints a string of args to stdout
+* @format: a list  of the arguments
+* Returns a string of chars
+*/
+
+int _printf(const char *format, ...)
+{
+
+}
