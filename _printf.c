4d147945eb5ca9af768853dd7a0e0a918f34b785
Derrick Ampire <ampire90@gmail.com>
2021-07-27 13:42:51 -0700

    Printf Function

--- _printf.c
+++ _printf.c
@@ -36,33 +36,33 @@ static int (*check_for_specs(const char *format))(va_list)
  */
 int _printf(const char *format, ...)
 {
-  unsigned int i = 0, count = 0;
-  va_list valist;
-  int (*f)(va_list);
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-  if (format == NULL)
-    return (-1);
+	if (format == NULL)
+		return (-1);
 
-  va_start(valist, format);
-  while (format[i])
-    {
-      for (; format[i] != '%' && format[i]; i++)
+	va_start(valist, format);
+	while (format[i])
 	{
-	  _putchar(format[i]);
-	  count++
-	}
-      if (!format[i])
-	return (count);
-      check = check_for_specs(&format[i + 1])
-	if (check != NULL)
-	  {
-	   count =+ check(valist);
-	   i += 2;
-	   continue;
-	  }
-      if (!format[i + 1})
-	return (-1);
-      _putchar(format[i]);
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specs(&format[i + 1]);
+		if (f != NULL)
+		{
+			count =+ f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1})
+			return (-1);
+		_putchar(format[i]);
 		count++;
 		if (format[i + 1] == '%')
 			i += 2;
